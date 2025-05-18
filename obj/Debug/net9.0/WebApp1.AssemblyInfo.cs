baab41a00f3ef97d12374d33ff566181f2668c1e
sudemzehracelikell <s.zehracelikel@gmail.com>
2025-05-18 14:05:20 +0300

    lk commit

--- obj/Debug/net9.0/WebApp1.AssemblyInfo.cs
+++ obj/Debug/net9.0/WebApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+baaaef501481111d26176c9fcf31d6809b05bd77")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
